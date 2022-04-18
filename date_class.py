779a44d82d9738dd1f8dc752d4d7ee4b897fbeae
SouravSaha00123 <85395921+SouravSaha00123@users.noreply.github.com>
2022-04-18 13:40:47 +0530

    Add files via upload

---
+++ date_class.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  8 00:25:16 2022
+
+@author: Sourav Saha
+"""
+import re
+
+class date_class():
+    def __init__(self,sentence='',path='E:/Python/Python codes(Spyder IDE)/date_pattern.txt'):
+        self.sentence = sentence
+        self.path = path        
+    
+    def date_extract(self):
+        fl=open(self.path,'r')
+        patterns=fl.read().strip('\n').split('\n') 
+        fl.close()
+        dates=[]
+        for pattern in patterns: 
+            # results= re.findall(r""+pattern+"",sentence)
+            results= re.findall(pattern,self.sentence) 
+            if results:
+               # print(results.group(0))
+                dates.append(''.join(results[0])) 
+                print(''.join(results[0]))            
+                print(pattern)
+         # input()
+        return dates
